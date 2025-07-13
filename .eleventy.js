c98993b128d85e9c605cf088572f533ff4cd8f40
Ondrej Peterka <ondrejpeterka@gmail.com>
2025-07-13 19:26:34 +0200

    druhý commit

--- .eleventy.js
+++ .eleventy.js
@@ -3,73 +3,75 @@ const CleanCSS = require("clean-css");
 const UglifyJS = require("uglify-js");
 const htmlmin = require("html-minifier");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const markdownIt = require("markdown-it");
+const markdownItAttrs = require("markdown-it-attrs");
+const markdownItForInline = require("markdown-it-for-inline");
+const markdownItAnchor = require("markdown-it-anchor");
 
 module.exports = function(eleventyConfig) {
-
-  // Eleventy Navigation https://www.11ty.dev/docs/plugins/navigation/
+  // --- PLUGINS ---
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-  // Configuration API: use eleventyConfig.addLayoutAlias(from, to) to add
-  // layout aliases! Say you have a bunch of existing content using
-  // layout: post. If you don’t want to rewrite all of those values, just map
-  // post to a new file like this:
-  // eleventyConfig.addLayoutAlias("post", "layouts/my_new_post_layout.njk");
+  // --- MARKDOWN ---
+  let md = markdownIt({ html: true, breaks: true, linkify: true })
+    .use(markdownItAttrs)
+    .use(markdownItAnchor, { permalink: false })
+    .use(markdownItForInline, 'image_linker', 'image', (tokens, idx) => {
+      const token = tokens[idx];
+      const src = token.attrGet('src');
+      const alt = token.content || '';
+      token.type = 'html_inline';
+      token.content = `<a href="${src}" class="gallery"><img src="${src}" alt="${alt}"></a>`;
+    });
 
-  // Merge data instead of overriding
-  // https://www.11ty.dev/docs/data-deep-merge/
+  eleventyConfig.setLibrary("md", md);
+
+  // --- COLLECTIONS ---
   eleventyConfig.setDataDeepMerge(true);
 
-  // Add support for maintenance-free post authors
-  // Adds an authors collection using the author key in our post frontmatter
-  // Thanks to @pdehaan: https://github.com/pdehaan
   eleventyConfig.addCollection("authors", collection => {
     const blogs = collection.getFilteredByGlob("posts/*.md");
     return blogs.reduce((coll, post) => {
       const author = post.data.author;
-      if (!author) {
-        return coll;
-      }
-      if (!coll.hasOwnProperty(author)) {
-        coll[author] = [];
-      }
+      if (!author) return coll;
+      if (!coll.hasOwnProperty(author)) coll[author] = [];
       coll[author].push(post.data);
       return coll;
     }, {});
   });
 
- eleventyConfig.addFilter("date", (value, format = "yyyy-MM-dd") => {
-    return DateTime.fromJSDate(value).toFormat(format);
-  });
-
-   // Tags
   eleventyConfig.addCollection("tagList", function (collectionApi) {
-  const tagSet = new Set();
-  collectionApi.getAll().forEach((item) => {
-    if ("tags" in item.data) {
-      let tags = item.data.tags;
-      tags = Array.isArray(tags) ? tags : [tags];
-      tags.forEach((tag) => tagSet.add(tag));
-    }
+    const tagSet = new Set();
+    collectionApi.getAll().forEach((item) => {
+      if ("tags" in item.data) {
+        let tags = Array.isArray(item.data.tags) ? item.data.tags : [item.data.tags];
+        tags.forEach((tag) => tagSet.add(tag));
+      }
+    });
+    return [...tagSet];
   });
-  return [...tagSet];
-});
 
-  // Date formatting (human readable)
+  // --- FILTERS ---
   eleventyConfig.addFilter("readableDate", dateObj => {
     return DateTime.fromJSDate(dateObj).toFormat("dd LLL yyyy");
   });
 
-  // Date formatting (machine readable)
   eleventyConfig.addFilter("machineDate", dateObj => {
     return DateTime.fromJSDate(dateObj).toFormat("yyyy-MM-dd");
   });
 
-  // Minify CSS
+  eleventyConfig.addFilter("czDate", dateObj => {
+    return new Intl.DateTimeFormat('cs-CZ', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    }).format(dateObj);
+  });
+
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
   });
 
-  // Minify JS
   eleventyConfig.addFilter("jsmin", function(code) {
     let minified = UglifyJS.minify(code);
     if (minified.error) {
@@ -79,51 +81,29 @@ module.exports = function(eleventyConfig) {
     return minified.code;
   });
 
-  // Minify HTML output
+  // --- TRANSFORMS ---
   eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
-    if (outputPath.indexOf(".html") > -1) {
-      let minified = htmlmin.minify(content, {
+    if (outputPath.endsWith(".html")) {
+      return htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
         collapseWhitespace: true
       });
-      return minified;
     }
     return content;
   });
 
-  // Don't process folders with static assets e.g. images
+  // --- STATIC FILES ---
   eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.addPassthroughCopy("static/img");
   eleventyConfig.addPassthroughCopy("admin/");
-  // We additionally output a copy of our CSS for use in Decap CMS previews
   eleventyConfig.addPassthroughCopy("_includes/assets/css/inline.css");
   eleventyConfig.addPassthroughCopy("images");
 
-  /* Markdown Plugins */
-  let markdownIt = require("markdown-it");
-  let markdownItAnchor = require("markdown-it-anchor");
-  let options = {
-    breaks: true,
-    linkify: true
-  };
-  let opts = {
-    permalink: false
-  };
-
-  eleventyConfig.setLibrary("md", markdownIt(options)
-    .use(markdownItAnchor, opts)
-  );
-
+  // --- RETURN CONFIG ---
   return {
     templateFormats: ["md", "njk", "liquid"],
-
-    // If your site lives in a different subdirectory, change this.
-    // Leading or trailing slashes are all normalized away, so don’t worry about it.
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for URLs (it does not affect your file structure)
     pathPrefix: "/",
-
     markdownTemplateEngine: "liquid",
     htmlTemplateEngine: "njk",
     dataTemplateEngine: "njk",
